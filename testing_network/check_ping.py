bdaacd968d664e9c47bdccfa442055c858a1b3be
zytha <zytha@KUWIN.local>
2024-02-02 16:05:10 +0900

    update save check ping

--- testing_network/check_ping.py
+++ testing_network/check_ping.py
@@ -1,4 +1,21 @@
 import pandas as pd 
+import os
 
 df = pd.read_excel('PiK8kE_IP_allocation.xlsx')
 print(df.head())
+
+IPs = df['IP address base']
+print(len(IPs))
+df.dropna(subset=['IP address base'], inplace=True)
+IPs = df['IP address base']
+print(len(IPs))
+
+def check_ping(hostname):
+    response = os.system("ping -c 1 " + hostname)
+    return response == 0
+
+file_o = open('check_ping.csv','w')
+file_o.write('IP,PING\n')
+for ip in IPs:
+    file_o.write(str(ip)+','+str(check_ping(ip))+'\n')
+file_o.close()
