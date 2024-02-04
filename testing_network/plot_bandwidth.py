a7921c140a5ae248649337c168da5564a70e4efe
K.Thonglek <thonglek.kundjanasith@osaka-u.ac.jp>
2024-02-04 17:48:23 +0900

    completed testing network

--- testing_network/plot_bandwidth.py
+++ testing_network/plot_bandwidth.py
@@ -11,4 +11,10 @@ plt.xlabel('TO 10.10.100.__')
 plt.xticks(rotation=90)
 plt.savefig('plot_bandwidth.png',bbox_inches='tight')
 
-# EXP
+# # EXP
+# US: 13, 14, 15, 16
+# JP: 34, 35, 36, 37, 38
+#     39, 40, 41, 42
+# TH: 65, 66, 68
+# AS: 113, 114, 115, 116,  
+#     117, 118, 119, 120, 121, 122
